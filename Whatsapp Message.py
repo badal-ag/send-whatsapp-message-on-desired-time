c6e4d880cf47aea02a32fa59b4e62d134e13d81f
Badal Agarwal <badal.agarwal1369@gmail.com>
2020-09-05 19:21:22 +0530

    Send Whatsapp Message on Desired Time

---
+++ Whatsapp Message.py
@@ -0,0 +1,14 @@
+# Hi, with this Code, you can send a WhatsApp Message to your Loved Ones very Easily on Time.
+
+# Points to Remember:
+# • You should be connected to the WhatsApp Web from the Number you want to send the Message.
+# • You should have an Internet Connection during the Process.
+# • It takes 130 Seconds to Open Whatsapp Web and 20 Seconds to Send the Message, so plan the time Accordingly. 
+# • The time is in 24 Hour Format.
+# Now Follow the Steps Below:
+# 1. Go to Command Prompt Terminal and write "pip install pywhatkit"
+# 2. After it is completed, then open Python Shell or VS Code and Copy the Code
+# 3. Then Simply Run the Code and see the Magic
+
+import pywhatkit
+pywhatkit.sendwhatmsg("+91897905XXXX","Hey, This is the Message sent to you.",19,11)
